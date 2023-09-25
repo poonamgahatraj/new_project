6fadaaa7f2f8c6cd033e8cd80c8173c7621db2eb
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-09-25 14:38:51 +0545

    new_project

---
+++ new_project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import firstpage from './pages/first-page.vue'
+</script>
+
+<template>
+  
+<firstpage/>
+ 
+</template>
+
+<style scoped>
+
+</style>
---
+++ new_project/src/pages/first-page.vue
@@ -0,0 +1,58 @@
+<template>
+    
+
+        <div class="container">
+            <div class="content">
+                <h1>Today</h1>
+                <p style="color:grey;">18th June 2019,Tuesday</p>
+
+                <div v-for= "item in tasks" class="task">
+                <h2>{{ item.title }}</h2>
+                <p style="color:grey">{{ item.description }}</p>
+                </div>
+            </div>
+        </div>
+
+   
+  
+</template>
+
+<style scoped>
+.container{
+    background:rgb(222, 193, 140);
+    
+    
+}
+
+.content{
+    background:white;
+    border-radius:10px;
+    margin:20px;
+   
+   
+   
+}
+
+.task{
+    background:white;
+    border-radius:20px;
+    border:1px solid grey;
+    margin-top:10px;
+}
+</style>
+
+<script>
+export default{
+    data(){
+        return{
+            tasks:[{title:"Project daily stand-up",description:"At the conference centre"},
+            {title:"Internia new UI style",description:"Remember to bring presents"},
+            {title:"Weekly review",description:"Wanda square E5"},
+            {title:"Interview ",description:"Remember to bring laptop "}
+        
+        ]
+        }
+
+    }
+}
+</script>
