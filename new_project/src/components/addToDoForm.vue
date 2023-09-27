0ce0114bdd2eb8b78d14c48e091c6063b9cde4b0
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-09-27 17:52:45 +0545

    new_project

--- new_project/src/components/addToDoForm.vue
+++ new_project/src/components/addToDoForm.vue
@@ -2,11 +2,29 @@
      <div class="todoform">
         <form style="    font-size: 20px;">
             <label>Title</label><br>
-            <input type="text" ><br>
+            <input v-model="form.title" type="text" ><br>
             <label>Description</label><br>
-            <input type="text" ><br><br>
-            <button>Save</button>
+            <input v-model="form.description" type="text" ><br><br>
+            
         </form>
+        <button @click="savetodo">Save</button>
         
     </div>
 </template>
+
+<script>
+export default{
+
+    data(){
+        return{
+            form:{title:"",description:""}
+        }
+    },
+    methods: {
+        savetodo(){
+            console.log("hello")
+            console.log(this.form)
+        }
+    }
+}
+</script>
--- new_project/src/pages/first-page.vue
+++ new_project/src/pages/first-page.vue
@@ -1,19 +1,34 @@
 <template>
     
 
-        <div class="container">
+        <div class="container" @click="closepopup">
 
                 <div v-if="showaddform" class="pop-up">
+                 <div style="display: flex;justify-content: space-between; align-items: center;">
+                    <h1>ToDoItems</h1>
+                    <button @click="closepopup" style="background: red; border: none;">X</button>
+                 </div>   
                 
-                    <addToDoForm/>
+             <div class="todoform">
+             <form style="    font-size: 20px;margin-top: 15%;">
+            <label>Title</label><br>
+            <input v-model="form.title" type="text" ><br>
+            <label>Description</label><br>
+            <input v-model="form.description" type="text" ><br><br>
+            
+             </form>
+             <button @click="savetodo" style="padding:10px;">Save</button>
+        
+             </div>
+                  
                 </div>
                    
                     
                 
                 <div class="content">
-                    <div style="padding: 20px">
-                        <h1 style="margin-left: 20px;;">Today</h1>
-                        <p style="color:grey;margin-left: 20px;">18th June 2019,Tuesday</p>
+                    <div style="padding: 15px;display: flex;flex-direction: column;align-items: flex-start;">
+                        <h1 >Today</h1>
+                        <p style="color:grey;margin:0;">18th June 2019,Tuesday</p>
                     </div>
                 
 
@@ -22,7 +37,7 @@
                         <h2 style="margin-left:20px;">{{ item.title }}</h2>
                         <p style="color:grey;margin-left:20px;">{{ item.description }}</p>
                     </div>
-                    <div >
+                    <div class="task2" >
 
                         <p style="color:grey">9:30 A.M.</p>
                         <img src="../assets/profile pic.jpg" class="image">
@@ -33,20 +48,22 @@
                      
            
                  </div>
-                 <div style="padding: 30px;" >
-                    <button @click="addtodoform" class="btn"> + Add new task</button>
-                 </div>
+                
           
         </div>
+
+        <div style="padding: 30px;" >
+                    <button @click="addtodoform" class="btn"> + Add new task</button>
+                 </div>
 
    
   
 </template>
 
 <script>
-import addToDoForm from '../components/addToDoForm.vue';
+
 export default{
-  components: { addToDoForm },
+  
     data(){
         return{
             tasks:[
@@ -54,11 +71,13 @@ export default{
             {title:"Internia new UI style",description:"Remember to bring presents"},
             {title:"Weekly review",description:"Wanda square E5"},
             {title:"Interview ",description:"Remember to bring laptop "},
+                 ],
+        showaddform:false,
+        form:{title:"",description:""}
 
-           
-        
-        ],
-        showaddform: false
+       
+
+
         }
    
 
@@ -69,7 +88,25 @@ export default{
     addtodoform()
     {
        
-        this.showaddform=!this.showaddform
+       
+        this.showaddform=true
+       
+    },
+
+    savetodo(){
+            console.log("hello")
+            console.log(this.form)
+            this.tasks.push(this.form)
+            this.showaddform=false
+        },
+
+    closepopup(){
+
+
+    //    if (this.showaddform ==true){
+           this.showaddform=false
+    //     }
+        
     }
 
     }
@@ -93,6 +130,7 @@ export default{
     background:white;
     border-radius:50px;
     margin:40px;
+    padding: 50px;
    
    
    
@@ -125,13 +163,12 @@ export default{
     height: 25px;
     width: 25px;
     border-radius: 25px;
-    margin-right: 20px;
-}
+    }
 
 .pop-up{
     
-    background:pink;
-    position:absolute;
+    background: pink;
+    position: absolute;
     left: 40%;
     bottom: 35%;
     height: 400px;
@@ -140,6 +177,20 @@ export default{
     
     
 
+}
+
+.task2{
+    margin-right: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+}
+
+.todoform{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  
 }
 </style>
 
